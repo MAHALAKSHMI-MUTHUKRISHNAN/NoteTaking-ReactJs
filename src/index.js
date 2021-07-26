9dbcdb9b5935de8289df202863b8bb699e75538f
MAHALAKSHMI M <66433024+MAHALAKSHMI-MUTHUKRISHNAN@users.noreply.github.com>
2021-07-26 21:52:47 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//var React = require("react");
+//var ReactDOM = require("react-dom");
+
+//newer version -
+import React from "react";
+import ReactDOM from "react-dom";
+
+var name = "maha";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>how are you?</p>
+  </div>,
+  document.getElementById("root")
+);
